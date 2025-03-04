0c1d983ebb5ceb981cf72af08e919b9c4918e58b
guizafj <fjd_90@hotmail.com>
2025-03-04 11:51:34 +0100

    V0.2

--- lab_bucle_adivinar_numero.py
+++ lab_bucle_adivinar_numero.py
@@ -1,5 +1,5 @@
-n_secret_number = 777
-n_usuario = int(input(
+ng_secret_number = 777
+ng_usuario = int(input(
     """
 +================================+
 | ¡Bienvenido a mi juego, muggle!|
@@ -11,10 +11,10 @@ n_usuario = int(input(
 ingresa tu opción aqui: """)
 )
 #cuando coincide el numero ingresado por el definido detiene el programa
-while n_usuario != 777: # se usa != para obtener una respuesta false 
-    if n_usuario != n_secret_number: # si no coincide imprime y solicita un nuevo numero
+while ng_usuario != 777: # se usa != para obtener una respuesta false 
+    if ng_usuario != ng_secret_number: # si no coincide imprime y solicita un nuevo numero
         print("¡Ja, ja! ¡Estás atrapado en mi bucle!")
-    n_usuario = int(input("intentalo de nuevo: ")) # si al ingresar un numero coincide con el predeterminado
+    ng_usuario = int(input("intentalo de nuevo: ")) # si al ingresar un numero coincide con el predeterminado
                                                 # detiene salta el bucle y ejecuta la siguiente orden
 
 print("¡Bien hecho, muggle! Eres libre ahora.")
